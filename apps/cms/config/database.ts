ef0dcafcabeb64b9e15ef24b56a4e7321fc5da8c
Steve Cox <stevejcox@me.com>
2022-10-18 19:33:31 -0700

    Add strapi cms

---
+++ apps/cms/config/database.ts
@@ -0,0 +1,13 @@
+export default ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', 'dpg-cd7lso82i3mt3nljllu0-a.oregon-postgres.render.com'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'cms_j953'),
+      user: env('DATABASE_USERNAME', 'cms_j953_user'),
+      password: env('DATABASE_PASSWORD', '9ffQs7xUyyrq3PDMFFN20bmtVWZC6M4f'),
+      ssl: env.bool('DATABASE_SSL', true),
+    },
+  },
+});
