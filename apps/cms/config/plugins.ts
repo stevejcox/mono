d7c24635f5f22375da7351ccbeb16649e5097b3d
Steve Cox <stevejcox@me.com>
2022-10-19 17:33:37 -0700

    Update plugins.ts

--- apps/cms/config/plugins.ts
+++ apps/cms/config/plugins.ts
@@ -1,4 +1,4 @@
-module.exports = ({ env }) => ({
+export default ({ env }) => ({
   upload: {
     config: {
       provider: 'cloudinary',
@@ -6,11 +6,6 @@ module.exports = ({ env }) => ({
         cloud_name: env('CLOUDINARY_NAME'),
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
       },
     },
   },
